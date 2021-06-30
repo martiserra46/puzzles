9b782137a6322774c1999cb259ce0ed338e8354e
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-06-30 16:44:40 +0200

    Setup InputElement functionality in input.cpp

--- input.cpp
+++ input.cpp
@@ -1,6 +1,9 @@
 #include "input.h"
 #include <iostream>
 #include <string>
+#include "text_utils.h"
+
+#define INPUT_MAX_LINE_WIDTH 30
 
 std::pair<std::string, Bundle> Input::input()
 {
@@ -25,4 +28,14 @@ std::pair<std::string, Bundle> Input::input()
     Bundle bundle = build_bundle(input_value);
 
     return std::pair<std::string, Bundle>(input_value, bundle);
+}
+
+InputElement::InputElement(std::string title) : title(title) {}
+
+std::string InputElement::get_text()
+{
+    std::string title_to_upper = to_uppercase(title);
+    std::string text = text_between_chars(title_to_upper, '-', INPUT_MAX_LINE_WIDTH) + "\n";
+    text += get_text_without_title() + "\n";
+    return text;
 }
