c8478e61e3cf0a7b3e8dd5dbf36ec1eafc2d0787
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-06-30 17:21:39 +0200

    Setup InputNumberValidator structure in input_validator.h

--- input_validator.h
+++ input_validator.h
@@ -8,4 +8,14 @@ class InputValidator
         virtual bool is_valid(std::string input_value) = 0;
 };
 
+class InputNumberValidator : public InputValidator
+{
+    public:
+        InputNumberValidator(int min, int max);
+        void set_min_max(int min, int max);
+        bool is_valid(std::string input_value);
+    private:
+        int min, max;
+};
+
 #endif
