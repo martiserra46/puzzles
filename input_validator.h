343d6db8304d7369eef33aa0bdc8404f871aa9a3
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-06-30 17:22:01 +0200

    Setup InputRowColumnValidator structure in input_validator.h

--- input_validator.h
+++ input_validator.h
@@ -1,6 +1,8 @@
 #ifndef INPUT_VALIDATOR_H
 #define INPUT_VALIDATOR_H
 
+#include <string>
+
 class InputValidator
 {
     public:
@@ -16,6 +18,16 @@ class InputNumberValidator : public InputValidator
         bool is_valid(std::string input_value);
     private:
         int min, max;
+};
+
+class InputRowColumnValidator : public InputValidator
+{
+    public:
+        InputRowColumnValidator(int min_row, int max_row, int min_col, int max_col);
+        void set_min_max_row_column(int min_row, int max_row, int min_col, int max_col);
+        bool is_valid(std::string input_value);
+    private:
+        int min_row, min_col, max_row, max_col;
 };
 
 #endif
