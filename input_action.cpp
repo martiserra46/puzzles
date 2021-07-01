c7fd3752f2c8eadb13290ff56fea97a3a2118503
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-01 09:39:11 +0200

    Setup InputAction functionality

---
+++ input_action.cpp
@@ -0,0 +1,12 @@
+#include "input_action.h"
+#include <string>
+#include "bundle.h"
+#include <utility>
+
+InputAction::InputAction(Input *input, Action *action) : input(input), action(action) {}
+
+void InputAction::do_input_action()
+{
+    std::pair<std::string, Bundle> input_value = input->input();
+    action->do_action(input_value.first, input_value.second);
+}
