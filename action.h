fd93aeaa5223fc8aa099f8d4cfb610340dbd314e
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-01 09:05:59 +0200

    Setup ActionGroup structure in action.h

--- action.h
+++ action.h
@@ -3,11 +3,19 @@
 
 #include <string>
 #include "bundle.h"
+#include <vector>
 
 class Action
 {
     public:
         virtual void do_action(std::string name, Bundle bundle) = 0;
+};
+
+class ActionGroup
+{
+    std::vector<Action*> actions;
+    public:
+        void do_action(std::string name, Bundle bundle);
 };
 
 #endif
