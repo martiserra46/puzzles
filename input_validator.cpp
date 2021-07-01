80265cddf184ba1da93a3dd72f3414a950025276
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-01 08:57:23 +0200

    Add comments in input_validator.cpp

--- input_validator.cpp
+++ input_validator.cpp
@@ -3,6 +3,7 @@
 #include "number_utils.h"
 #include "text_utils.h"
 
+/** InputNumberValidator **/
 InputNumberValidator::InputNumberValidator(int min, int max)
 {
     set_min_max(min, max);
@@ -22,6 +23,7 @@ bool InputNumberValidator::is_valid(std::string input_value)
     return true;
 }
 
+/** InputTwoNumbersValidator **/
 InputTwoNumbersValidator::InputTwoNumbersValidator(int min_num_1, int max_num_1, int min_num_2, int max_num_2)
 {
     set_min_max_num_1_num_2(min_num_1, max_num_1, min_num_2, max_num_2);
