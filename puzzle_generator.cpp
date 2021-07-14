4fbc13ec6b24cbf5f25d091d9f7a6cfca6bf04de
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-14 15:12:46 +0200

    Setup generate_puzzle function code

--- puzzle_generator.cpp
+++ puzzle_generator.cpp
@@ -1,6 +1,20 @@
 #include "puzzle_generator.h"
+#include <vector>
+#include "number_utils.h"
 
 Puzzle PuzzleGenerator::generate_puzzle(int rows, int columns, int num_figures)
 {
-    return Puzzle(5, 5, {});
+    std::vector<std::vector<char>> matrix = generate_matrix(rows, columns, num_figures);
+    std::vector<Figure> figures = get_figures_from_matrix(matrix);
+    return Puzzle(rows, columns, figures);
+}
+
+std::vector<std::vector<char>> PuzzleGenerator::generate_matrix(int rows, int columns, int num_figures)
+{
+    return {};
+}
+
+std::vector<Figure> PuzzleGenerator::get_figures_from_matrix(std::vector<std::vector<char>> matrix)
+{
+    return {};
 }
