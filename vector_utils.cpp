f417f14d3935ce9a211f514761bf74644be9dd28
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-14 15:23:24 +0200

    Setup fill_matrix

--- vector_utils.cpp
+++ vector_utils.cpp
@@ -1,2 +1,14 @@
 #include "vector_utils.h"
 
+#include <vector>
+
+void fill_matrix(std::vector<std::vector<char>> matrix, char value)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            matrix[i][j] = value;
+        }
+    }
+}
--- vector_utils.h
+++ vector_utils.h
@@ -1,4 +1,8 @@
 #ifndef VECTOR_UTILS_H
 #define VECTOR_UTILS_H
 
+#include <vector>
+
+void fill_matrix(std::vector<std::vector<char>> matrix, char value);
+
 #endif
