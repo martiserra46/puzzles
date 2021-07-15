247614bd66d941bc710d47a09c391ecec621a901
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-15 11:19:27 +0200

    Setup PuzzlesFileManager class main structure

--- puzzles_file_manager.cpp
+++ puzzles_file_manager.cpp
@@ -1,2 +1,11 @@
 #include "puzzles_file_manager.h"
 
+void PuzzlesFileManager::save_puzzle(std::string difficulty, int level_number, Puzzle puzzle)
+{
+
+}
+
+Puzzle PuzzlesFileManager::load_puzzle(std::string difficulty, int level_number)
+{
+    return Puzzle(5,5,{});
+}
--- puzzles_file_manager.h
+++ puzzles_file_manager.h
@@ -1,6 +1,13 @@
 #ifndef PUZZLES_FILE_MANAGER_H
 #define PUZZLES_FILE_MANAGER_H
 
+#include "puzzle.h"
 
+class PuzzlesFileManager
+{
+    public:
+        static void save_puzzle(std::string difficulty, int level_number, Puzzle puzzle);
+        static Puzzle load_puzzle(std::string difficulty, int level_number);
+};
 
 #endif
