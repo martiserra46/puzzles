1ed14590e7bbdd7bf1bacf7f27bb7345dd904d18
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-15 17:43:33 +0200

    Modify game controller to use GameScreen

--- game_controller.cpp
+++ game_controller.cpp
@@ -3,21 +3,24 @@
 #include "puzzle_generator.h"
 #include "drawing_generator.h"
 #include "puzzle.h"
+#include "screen.h"
 #include <iostream>
 
 void GameController::play_selected_level(std::string difficulty, int level)
 {
     Puzzle puzzle = PuzzlesFileManager::load_puzzle(difficulty, level);
-    std::cout << DrawingGenerator::generate_drawing(puzzle) << std::endl;
+    GameScreen game_screen(puzzle);
+    game_screen.init();
 }
 
 void GameController::play_generated_puzzle(int rows, int columns, int num_figures)
 {
     Puzzle puzzle = PuzzleGenerator::generate_puzzle(rows, columns, num_figures);
-    std::cout << DrawingGenerator::generate_drawing(puzzle) << std::endl;
+    GameScreen game_screen(puzzle);
+    game_screen.init();
 }
 
 void GameController::play_puzzle(Puzzle puzzle)
 {
-
+    std::cout << DrawingGenerator::generate_drawing(puzzle) << std::endl;
 }
