106bdcedf7b3be9c8c4b14d56683b67216eb470e
Martí Serra Molina <martiserramolina@MacBook-MacBook-Pro-de-Marti.local>
2021-07-15 17:08:56 +0200

    Setup generate_levels functionality

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include "screen.h"
-#include "puzzle_generator.h"
-#include "puzzles_file_manager.h"
-#include "drawing_generator.h"
-#include <iostream>
 
 int main()
 {
-    Puzzle puzzle = PuzzleGenerator::generate_puzzle(5, 5, 3);
-    PuzzlesFileManager::save_puzzle("e", 1, puzzle);
-    Puzzle puzzle2 = PuzzlesFileManager::load_puzzle("e", 1);
-    std::cout << DrawingGenerator::generate_drawing(puzzle2) << std::endl;
+    Screen screen;
+    screen.init();
     return 0;
 }
